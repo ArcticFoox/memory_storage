be31abb8ff53b9f9adcb79ca150b5732deb5ff50
신종신 <jongsin16@naver.com>
2021-11-27 17:19:25 +0900

    Table관련 function 추가 및 FTL_write 오류 수정

--- sector.cpp
+++ sector.cpp
@@ -19,14 +19,16 @@ void Flash_write(vector<Sector>* memory, int num, char a){
 void Flash_write(vector<Sector>* memory, int num, char a, int index, bool vaild){
     if(memory->at(num).data != 0)
         cout << "error" << "\n";
-    else
+    else{
         memory->at(num).data = a;
         memory->at(num).index = num;
         memory->at(num).vaild = vaild;
+    }
 }
 
 void Flash_erase(vector<Sector>* memory, int num){
-    num = num * 32;
+    //num = num * 32;
+    num = num * 4; //for test
     for(int i = 0; i < 32; i++){
         memory->at(num + i).data = 0;
         memory->at(num).index = 0;
@@ -38,20 +40,34 @@ char Flash_read(vector<Sector>* memory, int num){
     return memory->at(num).data;
 }
 
+void Table_init(int Table[], int size){
+    for(int i = 0; i < size; i++){
+        Table[i] = 0;
+    }
+}
+
+void Print_table(int Table[], int size){
+    for(int i = 0; i < size; i++){
+        cout << Table[i] << "\n";
+    }
+}
 
 int init(int num){
     cout << num <<" megabytes \n";
-    return num * 2048 + 32;  // spare Block
+    //return num * 2048 + 32;  // 32 is spare Block
+    return num * 32 + 4; // for test
 }
 
-void FTL_write(vector<Sector>* memory, int *Table, int num, char a, int cnt, int size){
-    if(cnt == size - 1){
+void FTL_write(vector<Sector>* memory, int Table[], int num, char a, int &cnt, int size){  // 일어난 횟수 셀 수 있도록 수정해야함
+    if(cnt == size){
         int b_size = 0;
         int i = 0;
         cnt = 0;
-        while(b_size < size / 32){
+        // while(b_size < size / 32){ 
+        while(b_size < size / 4){ // for test
             int b_cnt = 0;
-            while(b_cnt < 32 && i != size - 1){
+            //while(b_cnt < 32 && i != size - 1){
+            while(b_cnt < 32 && i != size){ //for test
                 if(memory->at(i).vaild){
                     Flash_write(memory, size + b_cnt, memory->at(i).data, memory->at(i).index, true);
                     b_cnt++;
@@ -67,27 +83,35 @@ void FTL_write(vector<Sector>* memory, int *Table, int num, char a, int cnt, int
                     t++;
                 }
                 else{
-                    Flash_erase(memory, (size / 32));
+                    //Flash_erase(memory, (size / 32));
+                    Flash_erase(memory, (size / 4)); //for test
                     break;
                 }
             }
         }
     }
-    else{
-       if(Flash_read(memory, Table[num])){
-            memory->at(Table[num]).vaild = false;
-            Table[num] = cnt++;
+    else{  //1에는 접근이 안됨
+        if(cnt == 0){
+            Table[num] = cnt;
+            cnt++;
             Flash_write(memory, Table[num], a, num, true);
         }
-       else{
-            Table[num] = cnt++;
+        if(memory->at(Table[num]).data != 0 && num == memory->at(Table[num]).index){
+            memory->at(Table[num]).vaild = false;
+            Table[num] = cnt;
+            cnt++;
+            Flash_write(memory, Table[num], a, num, true);
+        }
+        else{
+            Table[num] = cnt;
+            cnt++;
             Flash_write(memory, Table[num], a, num, true);
         }
     }
 }
 
-void FTL_read(vector<Sector>* memory, int Table[], int num){
-    Flash_read(memory, Table[num]);
+void FTL_read(vector<Sector>* memory, int Table[], int num){ 
+    cout << Flash_read(memory, Table[num]) << "\n";
 }
 
 int main(){
@@ -96,7 +120,6 @@ int main(){
     char p;
     int size;
     int cnt = 0;
-    pair<int, int> div;
     while(true){
         string input;
         cin >> input;
@@ -113,12 +136,13 @@ int main(){
     }
 
     vector<Sector> memory(size);
-    int *Table = new int[size];
+    int *Table = new int[size];  // 몇개의 값은 0이 아닌 쓰레기 값이 저장됨
+    Table_init(Table, size);     // 0은 비어있는 것으로 취급
     while(true){
         cin >> c;
         if(c == 'w'){
             cin >> num >> p;
-            FTL_write(&memory, Table, num, p, cnt, size);
+            FTL_write(&memory, Table, num, p, cnt, size);  //cnt 인자를 reference 형태로 넘겨줘야 됨
         }
 
         else if(c == 'e'){
@@ -128,8 +152,13 @@ int main(){
 
         else if(c == 'r'){
             cin >> num;
-            cout << FTL_read << "\n";
+            FTL_read(&memory, Table, num);
         }
+
+        else if(c == 'p'){
+            Print_table(Table, size);
+        }
+
         //탈출
         else if(c == 'x'){
             break;
